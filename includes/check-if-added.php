ec3b514f27aa129f816135552a65100c7d9a719e
Roshan Pathak <74421028+roshan1690@users.noreply.github.com>
2021-06-01 14:05:18 +0530

    Add files via upload

---
+++ includes/check-if-added.php
@@ -0,0 +1,17 @@
+<?php
+
+function check_if_added_to_cart($item_id) {
+    $user_id = $_SESSION['user_id']; //We'll get the user_id from the session
+    require("common.php"); // connecting to the database
+
+    $query = "SELECT * FROM users_items WHERE item_id='$item_id' AND user_id ='$user_id' and status='Added to cart'";
+    $result = mysqli_query($con, $query) or die(mysqli_error($con));
+
+    if (mysqli_num_rows($result) >= 1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+?>
---
+++ lifestylestoresql.sql
@@ -0,0 +1,92 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `items` (`id`, `name`, `price`) VALUES
+(1, 'Canon EOS', 36000),
+(2, 'Nikon DSLR', 40000),
+(3, 'Sony DSLR', 45000),
+(4, 'Olympus DSLR', 50000),
+(5, 'Titan Model #301', 13000),
+(6, 'Titan Model #201', 3000),
+(7, 'HMT Milan', 8000),
+(8, 'Faber Luba #111', 18000),
+(9, 'H&W', 800),
+(10, 'Luis Phil', 1000),
+(11, 'Jhon Zok', 1500),
+(12, 'Jhalsani', 1300);
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `contact` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `city` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `address` varchar(255) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `users_items` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `status` enum('Added to cart','Confirm','','') CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_items`
+--
+ALTER TABLE `users_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users_items`
+--
+ALTER TABLE `users_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
